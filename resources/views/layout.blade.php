63af992e912f189d0e515b6117006dc84eb3ed3e
DanielT19 <42480363+DanielT19@users.noreply.github.com>
2018-09-17 12:50:56 -0500

    mid day

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -10,29 +10,66 @@
 	<header>
 		<?php function activeMenu($url){
 		return request()->is($url) ?  'active' : '';
-		}?>
-		<h1>{{ request()->is('/') ? 'Estas en el Home' : 'No estas en el home'}}</h1>
-		<nav>
-			<a class="{{ activeMenu('/') }}" 
-				href="{{ route('home') }}">Home</a>
-			<a class="{{ activeMenu('saludos*') }}" 
-				href="{{ route('saludos', 'Daniel') }}">Saludos</a>
-			<a class="{{ activeMenu('mensajes/create') }}" 
-				href="{{ route('mensajes.create') }}">Contactos</a>
-				@if(auth()->check())
+		}?>	        
+
+<nav class="navbar navbar-expand-lg navbar navbar-light" style="background-color: #e3f2fd;">
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav container mr-auto">
+    	<li class="nav-item active {{ activeMenu('/') }}"><a class="nav-link" href="{{ route('home') }}">Home</a>
+		</li>
+		<li class="nav-item active {{ activeMenu('saludos*') }}" ><a class="nav-link" href="{{ route('saludos', 'Daniel') }}">Saludos</a>
+		</li>
+		<li class="nav-item active {{ activeMenu('mensajes/create') }}" ><a class="nav-link" href="{{ route('mensajes.create') }}">Contactos</a>
+		</li>
+		@if(auth()->check())
 			<a class="{{ activeMenu('mensajes') }}" 
 				href="{{ route('mensajes.index') }}">Mensajes</a>
 				
-			<a href="/logout">Cerrar sesion de {{ auth()->user()->name }}</a>
+			
 				@endif
-				@if(auth()->guest())
-			<a class="{{ activeMenu('login') }}" 
-				href="/login">Login</a>
-				@endif
-		</nav>
+				
+		
+
+     {{--   <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          Dropdown
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+          <a class="dropdown-item" href="#">Action</a>
+          <a class="dropdown-item" href="#">Another action</a>
+          <div class="dropdown-divider"></div>
+          <a class="dropdown-item" href="#">Something else here</a>
+        </div>
+      </li>--}}
+		
+    </ul>
+    <ul class="navbar-nav">
+        @if(auth()->guest())
+    	<li class="nav-item active {{ activeMenu('login') }}">
+			<a class="nav-link" href="/login">Login</a>
+		</li>
+   		@else
+   		<li>
+   			<a href="/logout">Cerrar sesion de {{ auth()->user()->name }}</a>
+   		</li>
+   		@endif
+   	</ul>
+  </div>
+</nav> 
+
+
+
+		{{-- <h1>{{ request()->is('/') ? 'Estas en el Home' : 'No estas en el home'}}</h1> --}}
+		
 	</header>
-	@yield('contenido')
+	<div class="container">
+		@yield('contenido')
+		<hr>
 	<footer>Copyright {{ date('Y') }}</footer>
+
+	</div>
+	
 
 </body>
 </html>
--- resources/views/messages/create.blade.php
+++ resources/views/messages/create.blade.php
@@ -12,20 +12,20 @@
 	{!! csrf_field() !!}
 	<p><label for="nombre">
 		Nombre
-		<input type="text" name="nombre" value="{{ old('nombre') }}">
+		<input class="form-control"  type="text" name="nombre" value="{{ old('nombre') }}">
 		{!! $errors->first('nombre','<span class=error>:message</span>')  !!}
 	</label></p>
 	<p><label for="email">
 		Email
-		<input type="email" name="email" value="{{ old('email') }}">
+		<input class="form-control" type="email" name="email" value="{{ old('email') }}">
 		{!!  $errors->first('email','<span class=error>:message</span>') !!}
 	</label></p>
 	<p><label for="email">
 		Mensaje
-		<textarea name="mensaje">{{ old('mensaje') }}</textarea>
+		<textarea class="form-control" name="mensaje">{{ old('mensaje') }}</textarea>
 		{!! $errors->first('mensaje','<span class=error>:message</span>') !!}
 	</label></p>
-	<button class="btn">Enviar</button>
+	<button class="btn btn-primary ">Enviar</button>
 </form>
 @endif
 @endsection
